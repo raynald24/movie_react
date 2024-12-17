6eac4827c5ff21d35bd89bf15d12d8cc18d077b7
unknown <raynaldkalee24@gmail.com>
2024-12-17 17:05:22 +0700

    login page dulu, ntar lanjut

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,20 +1,28 @@
+import { FaUserAlt,FaLock } from "react-icons/fa";
+import './LoginPage.css';
 function Login() {
   return (
-    <div className="wrapper">
+    <div className="wrapper">   
         <form action="">
           <h1>Login</h1>
           <div className="input-box">
             <input type="text" placeholder='Username' required />
-            <input type="text" placeholder='Password' required />
+            <FaUserAlt className="icon"/>
           </div>
-
+          <div className="input-box">
+          <input type="text" placeholder='Password' required />
+          <FaLock className="icon"/>
+          </div>
           <div className="remember-forgot">
             <label> <input type="checkbox"/> Remember me </label>
-            <a href="#">FOrget Password</a>
+            <a href="#reset-password">Forget Password? </a>
           </div>
           <button type="submit">
             Login
           </button>
+          <div className="register-link">
+            <p>Don't you have an account? <a href="#register"> register here</a> </p>
+          </div>
         
         </form>
     </div>

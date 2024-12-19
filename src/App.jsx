6a6e5810736b3452e6a4f4bce39360a35ae2f222
raynald24-work <ray24.works@gmail.com>
2024-12-19 22:40:33 +0700

    pnmbahan login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import Login from './components/pages/LoginPage'
+import Register from './components/pages/RegisterPage'
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>} />
+    <Routes>
+        <Route exact path="/" element={<Navigate to = "Login" replace/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </BrowserRouter>
     </> 
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,16 +1,36 @@
 import { FaUserAlt,FaLock } from "react-icons/fa";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import './LoginPage.css';
-function Login() {
+
+ 
+const Login = () => {
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+  //menangkap value dari input username dan password
+  const onChangeUsername = (e) => {
+    const value = e.target.value
+    setUsername(value)
+  }
+  const onChangePassword = (e) => {
+    const value = e.target.value 
+    setPassword (value)
+  }
+
   return (
     <div className="wrapper">   
         <form action="">
           <h1>Login</h1>
           <div className="input-box">
-            <input type="text" placeholder='     Username' required />
+            <input 
+            type="text" 
+            placeholder="Username" 
+            value={username} 
+            onChange={onChangeUsername} />
             <FaUserAlt className="icon"/>
           </div>
           <div className="input-box">
-          <input type="text" placeholder='     Password' required />
+          <input type="password" placeholder='     Password' value={password} onChange={onChangePassword} required />
           <FaLock className="icon"/>
           </div>
           <div className="remember-forgot">
@@ -20,9 +40,12 @@ function Login() {
           <button type="submit">
             Login
           </button>
-          <div className="register-link">
-            <p>Don't you have an account? <a href="#register"> register here</a> </p>
-          </div>
+            <div className="register-link">
+              <p>
+                Don't you have an account? 
+                <Link to="/Register" className="pull-right" > register here</Link>
+              </p>
+            </div>
         
         </form>
     </div>
